2b1d10ca3d33876dc1b3c89e8143ec62418c9c94
iamroh192 <121214691+iamroh192@users.noreply.github.com>
2025-09-29 20:09:57 +0530

    Latest DB URL

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
     await mongoose.connect(
-        "mongodb+srv://rohithbalaga_db_user:KOmIQEgqddFvHXv2@cluster0.q7bb49t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        "mongodb+srv://rohithbalaga_db_user:KOmIQEgqddFvHXv2@cluster0.q7bb49t.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
         // "mongodb://localhost:27017/devTinder",
         // "mongodb://rohithbalaga_db_user:KOmIQEgqddFvHXv2@cluster0.q7bb49t.mongodb.net/devTinder",
         // "mongodb+srv://rohithbalaga_db_user:KOmIQEgqddFvHXv2@ac-todiwmt-shard-00-00.q7bb49t.mongodb.net:27017/?retryWrites=true&w=majority&appName=Cluster0"
